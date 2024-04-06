a84449226b10eae9c1cc5a95199774b9fb6de705
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-04-06 00:16:41 -0300

    update: setting port back to 3001

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const routes = require('./src/routes/index');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
-// const port = 3001;
-const second_port = 8080;
+const port = 3001;
+// const second_port = 8080;
 
 dotenv.config();
 
@@ -22,8 +22,8 @@ app.use(express.json())
 
 app.use('/', routes);
 
-app.listen(second_port, () => {
-  console.log('running on port 8080');
+app.listen(port, () => {
+  console.log('running on port 3001');
 })
  
 module.exports = app;
